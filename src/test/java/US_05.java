fb07a52f44f53ad10bbe4bc80c076c32436a173a
Gulumser Wilson <wilsongulumser@gmail.com>
2024-11-29 21:15:05 -0700

    US_05 Yapildi

--- src/test/java/US_05.java
+++ src/test/java/US_05.java
@@ -1,2 +1,30 @@
-public class US_05 {
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class US_05 extends Setup {
+
+    @Test
+    public void Task12() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("movie/top_rated?language=en-US&page=1")
+                .then()
+                .log().body()
+                .statusCode(200);
+
+    }
+
+    @Test
+    public void Task13() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("movie/upcoming?language=en-US&page=1")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
 }
