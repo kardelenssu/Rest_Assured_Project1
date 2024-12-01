def44d8312b48520ee1a9032a2273875f3b958fc
Gulumser Wilson <wilsongulumser@gmail.com>
2024-12-01 16:57:14 -0700

    US_03 and US_04 Completed

--- src/test/java/US_03.java
+++ src/test/java/US_03.java
@@ -1,2 +1,37 @@
-public class US_03 {
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class US_03 extends Setup {
+
+    @Test
+    public void TC08_GetWatchlistMovies() {
+
+        given()
+                .spec(requestSpec)
+                .pathParam("account_id", "21632548")
+
+                .when()
+                .get("account/{account_id}/watchlist/movies")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void TC09_GetMovieGenres() {
+
+        given()
+                .spec(requestSpec)
+
+                .when()
+                .get("genre/movie/list")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
 }
